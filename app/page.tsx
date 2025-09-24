d740463f81d3bec3976019ab6f7347a26fc16191
lincolngarcia <=>
2025-09-23 20:25:41 -0600

    Correct Index Screen

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import "./app.css"
 
 Amplify.configure(outputs);
 
@@ -14,8 +15,20 @@ const client = generateClient<Schema>();
 export default function App() {
   return (
     <main>
-      <div>
-        This site is now functional
+      <div className="large">
+        <p>Welcome to</p>
+        <div className="course">
+          <div className="courseName">Web<br />Programming</div>
+          <div className="courseNum">260</div>
+        </div>
+        <p id="serviceName">
+          <a href="https://learn.cs260.click">Learn</a>
+          <a href="https://simon.cs260.click">Simon</a>
+          <a href="https://startup.cs260.click">Startup</a>
+        </p>
+      </div>
+      <div className="small">
+        <a href="https://learn.cs260.click">260</a>
       </div>
     </main>
   );
