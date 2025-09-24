39ad285480f4985ae84bf6bf371611d589689fd3
lincolngarcia <=>
2025-09-24 17:34:53 -0600

    External to Inline Router Call

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,11 @@
 import { Router, useRouter } from "next/router"
 export default function Page() {
     const router = useRouter();
-    function toDashboard() {
-        router.push("/admin/dashboard");
-    }
     return (
         <div>
             <input placeholder="Username" /><br />
             <input placeholder="Password" type="password" /><br />
-            <button onClick={toDashboard}>Login</button><br />
+            <button onClick={() => router.push("/admin/dashboard")}>Login</button><br />
         </div>
     )
 }
