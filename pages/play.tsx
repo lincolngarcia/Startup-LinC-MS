eef21b9037f7d2513c7e48ba9ef876fee4168a99
lincolngarcia <=>
2025-10-07 19:02:36 -0600

    Changes

--- pages/play.tsx
+++ pages/play.tsx
@@ -1,92 +1,72 @@
+import "./play.css";
+import Head from "next/head";
+
 export default function Page() {
     return (
         <main>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Simon</title>
+                <link rel="icon" href="favicon.ico" />
 
-            <header>
-                <h1>Simon<sup>&reg;</sup></h1>
-                <nav>
-                    <menu>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/play">Play</a></li>
-                        <li><a href="/scores">Scores</a></li>
-                        <li><a href="/about">About</a></li>
-                    </menu>
-                </nav>
-                <hr />
-            </header>
+                <link rel="stylesheet" href="main.css" />
+                <link rel="stylesheet" href="play.css" />
 
-            <main>
-                <div className="players">
-                    Player:
-                    <span className="player-name">Mystery player</span>
-                </div>
-                <ul className="notification">
-                    <li className="player-name">Tim started a new game</li>
-                    <li className="player-name">Ada started a new game</li>
-                    <li className="player-name">Tim scored 337</li>
-                </ul>
+            </Head>
+            <div className="bg-dark text-light">
+                <header className="container-fluid">
+                    <nav className="navbar fixed-top navbar-dark">
+                        <a className="navbar-brand" href="#">Simon<sup>&reg;</sup></a>
+                        <menu className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="index.html">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="play">Play</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="scores">Scores</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="about">About</a>
+                            </li>
+                        </menu>
+                    </nav>
+                </header>
 
-                <br />
+                <main className="bg-secondary">
+                    <div className="players">
+                        Player
+                        <span className="player-name">Mystery player</span>
+                        <div id="player-messages">
+                            <div className="event"><span className="player-event">Linus</span> scored 377</div>
+                            <div className="event"><span className="player-event">Linus</span> started a new game</div>
+                            <div className="event"><span className="system-event">game</span> connected</div>
+                        </div>
+                    </div>
 
-                <div>
-                    <label htmlFor="count">Score</label>
-                    <input type="text" id="count" value="--" readOnly />
-                </div>
+                    <div className="game">
+                        <div className="button-container">
+                            <button className="button-top-left"></button>
+                            <button className="button-top-right"></button>
+                            <button className="button-bottom-left"></button>
+                            <button className="button-bottom-right"></button>
+                            <div className="controls center">
+                                <div className="game-name">Simon<sup>&reg;</sup></div>
+                                <div className="score center">--</div>
+                                <button className="btn btn-primary">Reset</button>
+                            </div>
+                        </div>
+                    </div>
+                </main>
 
-                <br />
-
-                <div>
-                    <button>Reset</button>
-                </div>
-
-                <br />
-
-                <div>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <button>
-                                        <svg aria-hidden="true" viewBox="0 0 100 100" height="100" width="100">
-                                            <path d="M 95,5 95,95 5,95 Q 5,5 95,5" fill="green" />
-                                        </svg>
-                                    </button>
-                                </td>
-                                <td>
-                                    <button>
-                                        <svg aria-hidden="true" viewBox="0 0 100 100" height="100" width="100">
-                                            <path d="M5,5 5,95 95,95 Q 95,5 5,5" fill="red" />
-                                        </svg>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <button>
-                                        <svg aria-hidden="true" viewBox="0 0 100 100" height="100" width="100">
-                                            <path d="M5,5 95,5 95,95 Q 5,95 5,5" fill="blue" />
-                                        </svg>
-                                    </button>
-                                </td>
-                                <td>
-                                    <button>
-                                        <svg aria-hidden="true" viewBox="0 0 100 100" height="100" width="100">
-                                            <path d="M95,5 5,5 5,95 Q 95,95 95,5" fill="yellow" />
-                                        </svg>
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </main>
-
-            <footer>
-                <hr />
-                <span className="text-reset">Author Name(s)</span>
-                <br />
-                <a href="https://github.com/webprogramming260/simon-html">GitHub</a>
-            </footer>
+                <footer className="bg-dark text-white-50">
+                    <div className="container-fluid">
+                        <span className="text-reset">Author Name(s)</span>
+                        <a className="text-reset" href="https://github.com/webprogramming260/simon-css">Source</a>
+                    </div>
+                </footer>
+            </div>
         </main>
     )
 }
