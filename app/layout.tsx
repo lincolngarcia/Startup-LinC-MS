3b943343f6d87fc0a44950ac58f8618ad7d1d598
lincolngarcia <=>
2025-10-07 19:27:54 -0600

    Bootstrap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+      <link rel="stylesheet" 
+      href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" 
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"  />
+      </Head>
       <body className={inter.className}>{children}
         <div>test successful</div>
       </body>
