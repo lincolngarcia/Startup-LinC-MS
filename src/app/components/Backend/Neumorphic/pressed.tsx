2a4966be922ce3e49ecde81b106216df7d38a9a9
lincolngarcia <=>
2025-10-09 11:41:48 -0600

    Default Behavior and Inactivity

--- src/app/components/Backend/Neumorphic/pressed.tsx
+++ src/app/components/Backend/Neumorphic/pressed.tsx
@@ -1,6 +1,9 @@
-export default function NeumorphicPressed({ children, className }: { children: React.ReactNode, className?: string }) {
+export default function NeumorphicPressed({ active = true, children, className = '' }: { active?: boolean, children: React.ReactNode, className?: string }) {
+    const activeClasses = "_p-1 _rounded-lg _transition-shadow _duration-[0.5s] _text-black _shadow-[inset_5px_5px_10px_#bebebe,inset_-5px_-5px_10px_#ffffff] _bg-adminGray";
+    const classes = [className, active ? activeClasses : ''].filter(Boolean).join(' ');
+
     return (
-        <div className={className + " _p-1 _rounded-lg _transition-shadow _duration-[0.5s] _text-black _shadow-[inset_5px_5px_10px_#bebebe,inset_-5px_-5px_10px_#ffffff] _bg-adminGray"}>
+        <div className={classes}>
             <span className="_p-1">
                 {children}
             </span>
