fa2e9ba47026501ff7ca1ab2941a29b9acc9a15c
lincolngarcia <=>
2025-10-09 11:46:05 -0600

    Styling Fixes for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,16 @@ import { useRouter } from 'next/navigation'
 
 function PostCard(post: Post) {
   return (
-    <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+    <div className="_mb-8">
+      <h2 className="_mb-1 _text-xl">
+        <Link href={post.url} className="_text-blue-700 _hover:text-blue-900 _dark:text-blue-400">
           {post.title}
         </Link>
       </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+      <time dateTime={post.date} className="_mb-2 _block _text-xs _text-gray-600">
         {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
-      <div className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      <div className="_text-sm [&>*]:_mb-3 [&>*:last-child]:_mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
     </div>
   )
 }
@@ -29,8 +29,8 @@ export default function Home() {
   }
   return (
     <div className="mx-auto max-w-xl py-8">
-      <div><button onClick={toLogin}>Login-admin</button></div>
-      <h1 className="mb-8 text-center text-2xl font-black">Next.js + Contentlayer Example</h1>
+      <div><button className="_text-xl _border-2 _p-1 _m-1 _bg-adminBlue" onClick={toLogin}>Login-admin</button></div>
+      <h1 className="_mb-8 _text-center _text-2xl _font-black">Next.js + Contentlayer Example</h1>
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
       ))}
