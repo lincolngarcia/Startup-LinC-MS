989623f7ef8620ac7f306d55aa530034cbe6466b
lincolngarcia <=>
2025-10-09 11:41:59 -0600

    More Styling

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -4,41 +4,45 @@ import RootLayout from '../layout';
 // Import Components
 import Grid from '../../app/components/Backend/goldengrid';
 import NeumorphicPressed from '../../app/components/Backend/Neumorphic/pressed';
+import NeumorphicFlat from '@/src/app/components/Backend/Neumorphic/flat';
 
 export default function Page() {
     const router = useRouter();
 
     return (
         <Grid className="_h-screen _bg-adminGray">
-            <nav className="md:_col-span-2 lg:_col-span-3 _p-4 _bg-white _rounded-lg _shadow-md">
-                <ul>
-                    <li><NeumorphicPressed className="_w-4/5 text-center">Home</NeumorphicPressed></li>
-                    <li><a href="/admin/analytics">Analytics</a></li>
-                    <li>Pages</li>
-                    <li>Settings</li>
-                </ul>
-            </nav>
-            <Grid className="md:_col-start-3 md:_col-end-9 lg:_col-start-4 lg:_col-end-13">
-                <div className="md:_col-span-5">
+            <NeumorphicFlat className="_col-start-1 _col-end-3 _p-4 _bg-white">
+                <nav>
+                    <ul className="_text-xl">
+                        <li className="_my-2"><NeumorphicPressed className="_w-4/5 text-center">Home</NeumorphicPressed></li>
+                        <li className="_my-2"><NeumorphicPressed active={false} ><a href="/admin/analytics">Analytics</a></NeumorphicPressed></li>
+                        <li className="_my-2"><NeumorphicPressed active={false} >Pages</NeumorphicPressed></li>
+                        <li className="_my-2"><NeumorphicPressed active={false} >Settings</NeumorphicPressed></li>
+                    </ul>
+                </nav>
+                </NeumorphicFlat>
+            <Grid className="_col-start-4 _col-end-13">
+                <div className="_col-span-5">
                     <h1>Welcome to the Dashboard</h1>
                     <p>This is the main content area. You can display reports, data visualizations, or page content here.</p>
                 </div>
 
-                <div className="md:_col-start-6 md:_col-span-4 lg:_col-start-8 _h-full">
+                <NeumorphicFlat className="_col-start-8 _col-end-13 _h-full">
                     <h3>Editor</h3>
                     <div className="_h-4/5">
                         <textarea className="_h-4/5 _w-full" placeholder="write content here"></textarea>
-                        <input placeholder="edit"></input><button>Upload</button>
+                        <input placeholder="edit"></input><NeumorphicFlat><button className="_w-full">upload</button></NeumorphicFlat>
                     </div>
                     <div className="_flex _justify-between _mt-2">
-                        <button>Edit Pages</button>
-                        <button>Delete Pages</button>
-                        <button>New Page</button>
+                        <NeumorphicFlat><button>Edit Pages</button></NeumorphicFlat>
+                        <NeumorphicFlat><button>Delete Pages</button></NeumorphicFlat>
+                        <NeumorphicFlat><button>New Page</button></NeumorphicFlat>
                     </div>
                     <div>
-                        <button onClick={() => router.push("/")}>View Live Site</button>
+                        <br />
+                        <NeumorphicFlat><button className="_w-full" onClick={() => router.push("/")}>View Live Site</button></NeumorphicFlat>
                     </div>
-                </div>
+                </NeumorphicFlat>
             </Grid>
         </Grid>
     )
