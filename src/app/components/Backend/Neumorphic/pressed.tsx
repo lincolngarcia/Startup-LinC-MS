d724eaee7263a68e8942eac5f83d603277bd2be4
lincolngarcia <=>
2025-10-09 10:56:40 -0600

    Neumorphic Pressed Class Name functions

--- src/app/components/Backend/Neumorphic/pressed.tsx
+++ src/app/components/Backend/Neumorphic/pressed.tsx
@@ -1,7 +1,9 @@
-export default function NeumorphicPressed({ children }: { children: React.ReactNode }) {
+export default function NeumorphicPressed({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
-        <div className="_p-1 _rounded-lg _transition-shadow _duration-[0.5s] _text-black _shadow-[inset_5px_5px_10px_#bebebe,inset_-5px_-5px_10px_#ffffff] _bg-adminGray">
-            {children}
-            </div>
+        <div className={className + " _p-1 _rounded-lg _transition-shadow _duration-[0.5s] _text-black _shadow-[inset_5px_5px_10px_#bebebe,inset_-5px_-5px_10px_#ffffff] _bg-adminGray"}>
+            <span className="_p-1">
+                {children}
+            </span>
+        </div>
     )
 }
