8d1e5df4067236292498c28b4734ee66a1c0941c
lincolngarcia <=>
2025-10-09 10:54:59 -0600

    Removed Uncessary Tags

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,8 +7,7 @@ export default function Page() {
 
     return (
         <div className="_h-screen _w-screen _bg-adminGray _flex _flex-col _justify-center _items-center">
-            <div className="_mb-[100px]">
-                <NeumorphicFlat>
+                <NeumorphicFlat className="_mb-[100px]">
                     <div className="_flex _flex-col _justify-center _items-center _gap-4">
                         <p className="_text-adminBlue _font-bold _text-xl _border-b _border-adminBlue">LinC-MS</p>
                         <input placeholder="Username" className="softShadow _text-p4 _rounded-lg _py-1 _px-4 _transition-shadow _duration-[0.5s] _text-black;" />
@@ -16,7 +15,6 @@ export default function Page() {
                         <button className="softShadow _w-full" onClick={() => router.push("/admin/dashboard")}>Login</button>
                     </div>
                 </NeumorphicFlat>
-            </div>
         </div>
     )
 }
