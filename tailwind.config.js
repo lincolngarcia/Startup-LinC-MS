7fe552bf113dfa6573206aaf84da115e8880554c
lincolngarcia <=>
2025-10-08 23:37:39 -0600

    Default Colors & Prefix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  prefix: "_",
   theme: {
     extend: {},
+    colors: {
+      adminSuperGray: "#777777",
+      adminDarkGray: "#ACACAC",
+      adminGray: "#e0e0e0",
+      adminBlue: "#1432FF",
+      adminRed: "#E84B57",
+    },
   },
   plugins: [],
 }
