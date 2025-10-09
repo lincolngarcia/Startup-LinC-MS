f60c062497bdeaaecdc622fe7fc1b7790f855579
lincolngarcia <=>
2025-10-09 10:56:22 -0600

    Moved CSS to external file

--- src/app/components/Backend/Neumorphic/flat.tsx
+++ src/app/components/Backend/Neumorphic/flat.tsx
@@ -3,17 +3,9 @@
 import { ReactNode } from 'react';
 
 export default function NeumorphicFlat({ children, className }: { children: ReactNode, className?: string }) {
-
-    const _neumorphicShadow = {
-        "boxShadow": "5px 5px 10px #bebebe, -5px -5px 10px#ffffff;"
-    }
-    //     .active {
-    //     @apply _shadow - [inset_5px_5px_10px_#bebebe, _inset_ - 5px_ - 5px_10px_#ffffff];
-    // }
-
-return (
-    <div className={className + " _p-4 _rounded-lg _transition-shadow _duration-[0.5s] _text-black _bg-adminGray _neumorphicShadow"} style={_neumorphicShadow}>
-        {children}
-    </div>
-)
+    return (
+        <div className={className + " _p-4 _rounded-lg _transition-shadow _duration-[0.5s] _text-black _bg-adminGray neumorphicShadow"}>
+                {children}
+        </div>
+    )
 }
