f364089072a0e53833edd0591ee73025a88b4693
Lincoln Garcia <lincoln.ross.garcia@gmail.com>
2025-10-14 16:02:09 -0600

    Default Grid Behavior

--- src/app/components/Backend/Helpers/goldengrid.tsx
+++ src/app/components/Backend/Helpers/goldengrid.tsx
@@ -1,4 +1,4 @@
-export default function Grid({ children, className}: { children: React.ReactNode, className?: string }) {
+export default function Grid({ children, className=""}: { children: React.ReactNode, className?: string }) {
     return (
         <div className={className + " _grid _mx-auto _auto-cols-fr _gap-4 _p-4 _grid-cols-2 md:_grid-cols-8 lg:_grid-cols-12"}>
             {children}
