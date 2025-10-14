a539314c954932f2b395ba2b3bc37f3f0f0de5fe
Lincoln Garcia <lincoln.ross.garcia@gmail.com>
2025-10-14 15:54:19 -0600

    Golden Grid is Fixed Now Apparently

--- src/app/components/Backend/Helpers/goldengrid.tsx
+++ src/app/components/Backend/Helpers/goldengrid.tsx
@@ -1,6 +1,6 @@
 export default function Grid({ children, className}: { children: React.ReactNode, className?: string }) {
     return (
-        <div className={className + " _grid _mx-auto _grid-cols-12 _gap-4 _p-4"}>
+        <div className={className + " _grid _mx-auto _gap-4 _p-4 _grid-cols-2 md:_grid-cols-8 lg:_grid-cols-12"}>
             {children}
         </div>
     )
