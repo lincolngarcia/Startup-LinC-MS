ec3b05c6beca3f1f8ec89bfd2a2c422d5ddce394
Lincoln Garcia <lincoln.ross.garcia@gmail.com>
2025-10-16 20:59:57 -0600

    Change format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,34 @@
-"use client";
+import RootLayout from "../pages/layout"
+import Standard_FeatureStacked from "./components/Standard/Feature/Stacked";
+import Standard_FeatureStackedCard from "./components/Standard/Feature/StackedCard";
 
-import Link from 'next/link'
-import { compareDesc, format, parseISO } from 'date-fns'
-import { allPosts, Post } from 'contentlayer/generated'
-import { useRouter } from 'next/navigation'
-
-function PostCard(post: Post) {
-  return (
-    <div className="_mb-8">
-      <h2 className="_mb-1 _text-xl">
-        <Link href={post.url} className="_text-blue-700 _hover:text-blue-900 _dark:text-blue-400">
-          {post.title}
-        </Link>
-      </h2>
-      <time dateTime={post.date} className="_mb-2 _block _text-xs _text-gray-600">
-        {format(parseISO(post.date), 'LLLL d, yyyy')}
-      </time>
-      <div className="_text-sm [&>*]:_mb-3 [&>*:last-child]:_mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
-    </div>
-  )
+export default function Page() {
+    return (
+        <div className="_min-w-screen">
+            <div className="_max-w-[1200px] _m-auto _p-4">
+                <Standard_FeatureStacked>
+                    <Standard_FeatureStackedCard
+                        image="/content-image.jpeg"
+                        title="Welcome to the Homepage"
+                        action="Learn more"
+                        description={<p>This is a description. It descripts. Que mas quiere que te diga?</p>}
+                    />
+                    <Standard_FeatureStackedCard
+                        image="/content-image.jpeg"
+                        title="Welcome to the Homepage"
+                        action="Learn more"
+                        description={<p>This is a description. It descripts. Que mas quiere que te diga?</p>}
+                    />
+                </Standard_FeatureStacked>
+            </div>
+        </div>
+    )
 }
 
-export default function Home() {
-  const router = useRouter();
-  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
-  function toLogin() {
-    router.push("/login");
-  }
-  return (
-    <div className="mx-auto max-w-xl py-8">
-      <div><button className="_text-xl _border-2 _p-1 _m-1 _bg-adminBlue" onClick={toLogin}>Login-admin</button></div>
-      <h1 className="_mb-8 _text-center _text-2xl _font-black">Next.js + Contentlayer Example</h1>
-      {posts.map((post, idx) => (
-        <PostCard key={idx} {...post} />
-      ))}
-      <h3>GITHUB LINK</h3>
-      <a href="https://github.com/lincolngarcia/Startup-LinC-MS">Lincoln Garcia</a>
-    </div>
-  )
+Page.getLayout = function getLayout(page: React.ReactNode) {
+    return (
+        <RootLayout>
+            {page}
+        </RootLayout>
+    )
 }
