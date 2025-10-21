390e028ee99a6445aa3304bf1d388348b866d521
Lincoln Garcia <lincoln.ross.garcia@gmail.com>
2025-10-21 12:54:48 -0600

    Golden Grid Fixes

--- src/app/components/Backend/Helpers/goldengrid.tsx
+++ src/app/components/Backend/Helpers/goldengrid.tsx
@@ -1,6 +1,6 @@
 export default function Grid({ children, className = "" }: { children: React.ReactNode, className?: string }) {
     const defaultclasses = "grid mx-auto auto-rows-fr auto-cols-fr gap-4 p-4 grid-cols-4 md:grid-cols-8 lg:grid-cols-12";
-    const childstyling = "[&>*]:min-h-0";
+    const childstyling = "";
     const classes = [defaultclasses, childstyling, className].join(" ").trim();
 
     return (
