3b0caf37030a4dd68d00272eafc1420b98781028
Lincoln <lincoln.ross.garcia@gmail.com>
2025-10-30 15:45:10 -0600

    DynamicRender Component

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,8 +1,7 @@
-
-
 // Import Component Libraries
 import { useEffect, useState } from "react"
-import StandardExports from "../components/Standard/Standard"
+
+import DynamicRender from "@/src/app/components/Backend/Helpers/dyanmicrender"
 
 import pagedata from "@/database/_.json"
 
@@ -10,27 +9,10 @@ import RootLayout from "../../pages/layout"
 import "../../app/global.css"
 
 export default function Page() {
-    const library: any = {
-        ...StandardExports
-    }
-
-    function renderChildren(parent: any, parentKey="component-"): React.ReactNode {
-        console.log(`rendering parent:`, parent)
-        return parent.map((component: any, index: number) => {
-            const Tag = library[component.componentTag];
-            const key = `${parentKey}-${index}`;
-            if (Tag) {
-                return <Tag key={key} {...component.props}>
-                    {parent.map((child: any) => renderChildren(child.children, key))}
-                </Tag>
-            }
-        })
-    }
-
     return (
         <div className="min-w-screen">
             <div className="max-w-[1200px] m-auto p-4">
-                {renderChildren(pagedata.children)}
+                {DynamicRender(pagedata.children)}
             </div>
         </div>
     )
