8e9d83f7c1d0b57ca52cd32256bd1268b975cfc8
lincolngarcia <=>
2025-11-07 19:05:53 -0700

    Type Casting

--- src/pages/admin/_modals/pageSelectorModal.tsx
+++ src/pages/admin/_modals/pageSelectorModal.tsx
@@ -1,18 +1,26 @@
 import BackendModal from "@/src/app/components/Backend/modal"
+import { useEffect } from "react"
 
 export default function BackendPageSelectorModal({render, renderModal, context}: {render:any, renderModal: any, context: any}) {
     function handleSelection(key: any) {
         context.setPagedata(context.PageDB[key])
         renderModal(false)
     }
-    return (
-        <BackendModal render={render} renderModal={renderModal}>
+
+    useEffect(() => {
+        console.log(context.PageDB)
+        Object.entries(context.PageDB).map((entry: any) => console.log("index", entry[0], entry[1]))
+        console.log("end object entires")
+    }, [])
+
+        return (
+            <BackendModal render={render} renderModal={renderModal}>
             <div>
                 <h2 className="text-2xl font-semibold mb-4">Page Selection</h2>
                 <ol>
-                    {Object.entries(context.PageDB).map(([key, page]: any) => {
-                        return <li className="w-full flex flex-row justify-between">{page.title}<button onClick={() => handleSelection(key)}>useme!</button></li>
-                    })}
+                    {context && Object.entries(context.PageDB).map(([key, page]: any) => {
+                        return <li key={"selector-modal-" + key} className="w-full flex flex-row justify-between">{page.title}<button onClick={() => handleSelection(key)}>useme!</button></li>
+                        })}
                 </ol>
             </div>
         </BackendModal>
